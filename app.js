0e29c75fef8e41b11e6f684acd441b2fef86fa1c
lewi01 <isslewi03@gmail.com>
2021-12-22 22:06:34 +0300

    To Do List

---
+++ app.js
@@ -0,0 +1,8 @@
+let inputTask =document.querySelector("#input-task");
+let taskAdd =document.querySelector("#add-task-button");
+let li =document.querySelector("#task-list");
+let deleteBtn =document.querySelector("#delete-btn");
+
+
+
+
